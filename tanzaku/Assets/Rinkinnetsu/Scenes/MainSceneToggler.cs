f32b1a43bf2c0b13cda4bc2c019fce17cfd7f0f7
kiki344 <game22403019@gmail.com>
2025-11-06 11:42:37 +0900

    4
    クエスト

---
+++ tanzaku/Assets/Rinkinnetsu/Scenes/MainSceneToggler.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneToggler : MonoBehaviour
+{
+    [Header("要叠加的场景名称")]
+    public string sceneName = "MainScene"; // 这里写要叠加的场景名
+
+    private bool isSceneLoaded = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (!isSceneLoaded)
+            {
+                LoadSceneAdditively();
+            }
+            else
+            {
+                UnloadScene();
+            }
+        }
+    }
+
+    void LoadSceneAdditively()
+    {
+        if (!isSceneLoaded)
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+            isSceneLoaded = true;
+            Debug.Log($"已叠加加载场景: {sceneName}");
+        }
+    }
+
+    void UnloadScene()
+    {
+        if (isSceneLoaded)
+        {
+            SceneManager.UnloadSceneAsync(sceneName);
+            isSceneLoaded = false;
+            Debug.Log($"已卸载叠加场景: {sceneName}");
+        }
+    }
+}
